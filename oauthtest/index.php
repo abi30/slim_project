d6d370caa62c2b4e18f63eaf8c6439b67faafd62
abi30 <rakib0751@gmail.com>
2022-05-20 00:25:32 +0200

    test work done

--- oauthtest/index.php
+++ oauthtest/index.php
@@ -1,43 +1,73 @@
 <?php
-$client_id = '805b2cf4-f4c7-441c-a45c-17f6a719eeed';
-$client_secret = 'l7K3xVl_IeK7xnxBKihO04P8LR5saySx4tx2_kCI40JMxA0e1sFHFbf1VzrqAIcu3ZLdOjNaVKl16Ujt';
-$redirect_uri = 'https://connect.arisecur.com/oauth/callback';
-$scope = 'ameise/mitarbeiterwebservice';
-$state = "abc12345555";
 
-$url = "https://auth.dionera.com/oauth2/auth?client_id=$client_id&client_secret=$client_secret&state=$state&redirect_uri=$redirect_uri&scope=$scope&response_type=code";
+$token_url = "https://auth.dionera.com/oauth2/token";
 
-if (!isset($_GET['code'])) {
-    echo '<a href="' . $url . '">Login ameise</a>';
+$test_api_url = "https://www.maklerinfo.biz/";
+
+//    client (application) credentials on apim.byu.edu
+$client_id = "805b2cf4-f4c7-441c-a45c-17f6a719eeed";
+$client_secret = "l7K3xVl_IeK7xnxBKihO04P8LR5saySx4tx2_kCI40JMxA0e1sFHFbf1VzrqAIcu3ZLdOjNaVKl16Ujt";
+
+
+$access_token = getAccessToken();
+echo $access_token;
+$resource = getResource( $access_token );
+echo $resource;
+
+//    step A, B - single call with client credentials as the basic auth header
+//        will return access_token
+function getAccessToken()
+{
+    global $token_url, $client_id, $client_secret;
+
+    $content = "grant_type=client_credentials&scope=ameise/mitarbeiterwebservice";
+    $authorization = base64_encode( "$client_id:$client_secret" );
+   
+    $header = array( "Authorization: Basic {$authorization}", "Content-Type: application/x-www-form-urlencoded" );
+
+    $curl = curl_init();
+
+
+    curl_setopt_array( $curl, array(
+        CURLOPT_URL            => $token_url,
+        CURLOPT_HTTPHEADER     => $header,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST           => true,
+        CURLOPT_POSTFIELDS     => $content,
+    ) );
+    $response = curl_exec( $curl );
+    $err = curl_error( $curl );
+
+        
+    
+    curl_close( $curl );
+    if ( $err ) {
+    echo "cURL Error #:" . $err;
 } else {
-    $code = $_GET['code'];
+    echo $response;
+}
 
-    $apiData = array(
-        'client_id'       => $client_id,
-        'client_secret'   => $client_secret,
-        'grant_type'      => 'authorization_code',
-        'redirect_uri'    => $redirect_uri,
-        'code'            => $code,
-        'State'           => 'abc12345555'
-    );
 
-    $apiHost = 'https://auth.dionera.com/oauth2/token';
+    return json_decode( $response )->access_token;
+}
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $apiHost);
-    curl_setopt($ch, CURLOPT_POST, count($apiData));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($apiData));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    $jsonData = curl_exec($ch);
-    curl_close($ch);
+//    step B - with the returned access_token we can make as many calls as we want
+function getResource( $access_token )
+{
+    global $test_api_url;
 
-    var_dump($jsonData);
-    $user = @json_decode($jsonData);
+    $header = array( "Authorization: Bearer {$access_token}" );
 
-    echo '<pre>';
-    print_r($user);
-    exit;
+    $curl = curl_init();
+    curl_setopt_array( $curl, array(
+        CURLOPT_URL            => $test_api_url,
+        CURLOPT_HTTPHEADER     => $header,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+    ) );
+    $response = curl_exec( $curl );
+    curl_close( $curl );
+
+    return json_decode( $response, true );
 }
--- oauthtest/index43.php
+++ oauthtest/index43.php
@@ -3,8 +3,9 @@ $client_id = '805b2cf4-f4c7-441c-a45c-17f6a719eeed';
 $client_secret = 'l7K3xVl_IeK7xnxBKihO04P8LR5saySx4tx2_kCI40JMxA0e1sFHFbf1VzrqAIcu3ZLdOjNaVKl16Ujt';
 $redirect_uri = 'https://connect.arisecur.com/oauth/callback';
 $scope = 'ameise/mitarbeiterwebservice';
+$state = "abc12345555";
 
-$url = "https://api.instagram.com/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scope&response_type=code";
+$url = "https://auth.dionera.com/oauth2/auth?client_id=$client_id&client_secret=$client_secret&state=$state&redirect_uri=$redirect_uri&scope=$scope&response_type=code";
 
 if (!isset($_GET['code'])) {
     echo '<a href="' . $url . '">Login ameise</a>';
@@ -16,7 +17,8 @@ if (!isset($_GET['code'])) {
         'client_secret'   => $client_secret,
         'grant_type'      => 'client_credentials',
         'redirect_uri'    => $redirect_uri,
-        'code'            => $code
+        'code'            => $code,
+        'State'           => 'abc12345555'
     );
 
     $apiHost = 'https://auth.dionera.com/oauth2/token';
@@ -27,6 +29,8 @@ if (!isset($_GET['code'])) {
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($apiData));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     $jsonData = curl_exec($ch);
     curl_close($ch);
 
