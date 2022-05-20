70acfb9e536d7baefbe81759d13a8a04b44bf6d3
Abdulla al Rakib <abdulla.rakib@work.local>
2022-05-20 15:00:54 +0200

    test

--- oauthtest/index1.php
+++ oauthtest/index1.php
@@ -1,2 +1,9 @@
-<a href="https://connect.squareup.com/oauth2/authorize?client_id=sq0idp-gbQhcOCpmb2X4W1588Ky7A">Authorize App</a>
 <a href="https://auth.dionera.com/oauth2/authorize?client_id=805b2cf4-f4c7-441c-a45c-17f6a719eeed">Ameime App</a>
+<a href="https://auth.dionera.com/oauth2/authorize?client_id=805b2cf4-f4c7-441c-a45c-17f6a719eeed">test Amise</a>
+<?php
+
+client_secret=$client_secret&
+$url = "https://auth.dionera.com/oauth2/auth?client_id=$client_id&state=$state&redirect_uri=$redirect_uri&scope=$scope&response_type=code&token_endpoint_auth_method=client_secret_basic";
+
+<a href="' . $url . '">Login ameise</a>
+?>
